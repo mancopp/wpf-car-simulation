c80523eff0f1e5900b91a2d7ab6dc08cce0d457c
mancopp <mancoppor@gmail.com>
2023-01-11 19:01:27 +0100

    Add car movement basics

--- Car.cs
+++ Car.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -12,13 +11,40 @@ namespace wpf_car_simulation
     public class Car
     {
         private readonly string imgPath = "Resources/car.png";
+        private Canvas mainCanvas;
         private int id = 1;
         private int velocity = 1;
         private bool directedToRight = true;
+        private Image carSprite;
+        private int _rotateTransformAngle = 0;
 
-        public Car(Canvas myCanvas)
+        public Car(Canvas canvas)
         {
-            int top = 245;
+            this.mainCanvas = canvas;
+        }
+
+        public void spawnStatic()
+        {
+            int top = 310;
+            int left = 30;
+
+            System.Windows.Controls.Image carSprite = new System.Windows.Controls.Image();
+            carSprite.Source = new BitmapImage(new Uri(@"/wpf-car-simulation;component/Resources/car.png", UriKind.Relative));
+            carSprite.Height = 50;
+
+            this.carSprite = carSprite;
+
+            SetRotation(-90);
+
+            this.mainCanvas.Children.Add(this.carSprite);
+            Canvas.SetTop(this.carSprite, top);
+            Canvas.SetLeft(this.carSprite, left);
+        }
+
+        public void startThread()
+        {
+
+            int top = 180;
             int left = 30;
 
             System.Windows.Controls.Image carSprite = new System.Windows.Controls.Image();
@@ -26,14 +52,45 @@ namespace wpf_car_simulation
             carSprite.Width = 80;
             carSprite.Height = 80;
             RotateTransform rt = new RotateTransform(-90);
-            rt.CenterX = 40;
-            rt.CenterY = 40;
+            rt.CenterX = 25;
+            rt.CenterY = 25;
 
             carSprite.RenderTransform = rt;
 
-            myCanvas.Children.Add(carSprite);
+            this.mainCanvas.Children.Add(carSprite);
             Canvas.SetTop(carSprite, top);
             Canvas.SetLeft(carSprite, left);
+
+        }
+
+        public void SetPos(int top, int left)
+        {
+            Canvas.SetTop(carSprite, top);
+            Canvas.SetLeft(carSprite, left);
+        }
+
+        public void OffsetPos(int topOff, int leftOff)
+        {
+            Canvas.SetTop(carSprite, Canvas.GetTop(carSprite) + topOff);
+            Canvas.SetLeft(carSprite, Canvas.GetLeft(carSprite) + leftOff);
+        }
+
+        public void SetRotation(int rot)
+        {
+            _rotateTransformAngle = rot;
+            RotateTransform rt = new RotateTransform(_rotateTransformAngle);
+            rt.CenterX = 25;
+            rt.CenterX = 25;
+            carSprite.RenderTransform = rt;
+        }
+
+        public void Rotate(int rot)
+        {
+            _rotateTransformAngle += rot;
+            RotateTransform rt = new RotateTransform(_rotateTransformAngle);
+            rt.CenterX = 25;
+            rt.CenterY = 25;
+            carSprite.RenderTransform = rt;
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,15 +21,59 @@ namespace wpf_car_simulation
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Car car;
+
         public MainWindow()
         {
             InitializeComponent();
             Car car = new Car(myCanvas);
+            car.spawnStatic();
+            this.car = car;
+
+            this.DataContext = car;
         }
 
         public void Output(string str)
         {
             MessageBox.Show(str);
+        }
+
+        private void moveCarThr()
+        {
+            for (int i = 0; i < 360*100; i++)
+            {
+                this.Dispatcher.Invoke(() => car.Rotate(1));
+                Thread.Sleep(1);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new Thread(() => moveCarThr()).Start();
+        }
+
+        public void StandardRoute()
+        {
+            for (int i = 0; i < 650; i++)
+            {
+                this.Dispatcher.Invoke(() => car.OffsetPos(0, 1));
+                Thread.Sleep(1);
+            }
+
+            //Check if redlight
+            Thread.Sleep(500);
+
+            for (int i = 0; i < 120; i++)
+            {
+                this.Dispatcher.Invoke(() => car.OffsetPos(0, 1));
+                Thread.Sleep(1);
+            }
+
+            Thread.Sleep(500);
+
+            this.Dispatcher.Invoke(() => car.SetRotation(0));
+            Thread.Sleep(1000);
+            this.Dispatcher.Invoke(() => car.OffsetPos(70, 70));
         }
     }
 }
