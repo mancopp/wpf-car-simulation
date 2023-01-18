7ebb308d922458e45080a74e4b9312afd9149147
mancopp <mancoppor@gmail.com>
2023-01-18 20:03:20 +0100

    Small changes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -115,12 +115,7 @@ namespace wpf_car_simulation
                 counterLeft++;
                 CarListLeft[counterLeft].velocity = velocity;
                 CarListLeft[counterLeft].spawnStatic(directionRight);
-                if (counterLeft > 1)
-                { new Thread(() => RouteLeft(CarListLeft[counterLeft], CarListLeft[counterLeft - 1])).Start(); } 
-                else  {
-
-                    new Thread(() => RouteLeft(CarListLeft[counterLeft], CarListLeft[counterLeft])).Start();
-                }
+                new Thread(() => RouteLeft(CarListLeft[counterLeft])).Start();
             }
         }
 
@@ -202,7 +197,7 @@ namespace wpf_car_simulation
         }
 
 
-        public void RouteLeft(Car car, Car car2)
+        public void RouteLeft(Car car)
         {
            double x = 0, y = 0,r = 0;
 
@@ -231,18 +226,35 @@ namespace wpf_car_simulation
                 else if (1660 < i && i <= 2090) r = -0.418;
                 else r = 0;
 
-                ////1000<i<= y=-1 ; x=0;
-
                 car.position = i;
+                Car car2 = car;
+                if (CarListLeft.ElementAtOrDefault(car.id + 1) != null)
+                {
+                    car2 = CarListLeft[car.id + 1];
+                }
 
                 if (counterLeft > 1)
                 {
+
                     if (car2.position - car.position == 60)
                     {
-
-
+                        MessageBox.Show("Stop");
                         car.velocity = car2.velocity;
+                    }
+                }
 
+                if (i == 325 || i == 1550 || i == 2050)
+                {
+                    while (true)
+                    {
+                        if (carStop)
+                        {
+                            Thread.Sleep(500);
+                        }
+                        else
+                        {
+                            break;
+                        }
                     }
                 }
 
@@ -255,110 +267,12 @@ namespace wpf_car_simulation
                 CarListLeft.RemoveAt(car.id);
                 counterLeft--;
             }
-        
-            //for (int i = 0; i < 910; i++)
-            //{
-            //    this.Dispatcher.Invoke(() => car.OffsetPos(0, -1));
-            //    Thread.Sleep(car.velocity);
-            //       r =0
-            //}
-
-            ////910<i<=1000 y=-1 ; x=-1; r=
-            ///  //       r =1
-            //for (int i = 0; i < 90; i++)
-            //{
-            //    this.Dispatcher.Invoke(() => car.OffsetPos(-1, -1));
-            //    this.Dispatcher.Invoke(() => car.Rotate(1));
-
-            //    Thread.Sleep(car.velocity + 1);
-
-            //       r =0
-            //}
-
-            ////1000<i<=1030 y=-1 ; x=0;
-            ///  //       r =1
-            //for (int i = 0; i < 30; i++)
-            //{
-            //    this.Dispatcher.Invoke(() => car.OffsetPos(-1, 0));
-            //    Thread.Sleep(car.velocity + 1);
-            //}
-            ////1030<i<=1120 y=-1 ; x=1;
-            ///  //       r =1
-            //for (int i = 0; i < 90; i++)
-            //{
-            //    this.Dispatcher.Invoke(() => car.OffsetPos(-1, 1));
-            //    this.Dispatcher.Invoke(() => car.Rotate(1));
-            //    Thread.Sleep(car.velocity + 1);
-
-            //}
-            ////1120<i<=1660 y=0 ; x=1;
-            ///  //       r =0
-            //for (int i = 0; i < 540; i++)
-            //{
-            //    this.Dispatcher.Invoke(() => car.OffsetPos(0, 1));
-            //    Thread.Sleep(car.velocity);
-            //}
-            ////1660<i<=1760 y=-0.2; x=1;
-
-            //       r =-0.418
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    this.Dispatcher.Invoke(() => car.OffsetPos(-0.2, 1));
-            //    this.Dispatcher.Invoke(() => car.Rotate(-0.418));
-            //    Thread.Sleep(car.velocity+1);
-
-            //}
-            ////1760<i<=1830 y=-1; x=0.6;
-            /// //       r =-0.418
-            //for (int i = 0; i < 70; i++)
-            //{
-            //    this.Dispatcher.Invoke(() => car.OffsetPos(-1, 0.6));
-            //    this.Dispatcher.Invoke(() => car.Rotate(-0.418));
-            //    Thread.Sleep(car.velocity+1);
-
-            //}
-            ////1830<i<=1890 y=-1; x=0;
-            /// //       r =-0.418
-            //for (int i = 0; i < 60; i++)
-            //{
-            //    this.Dispatcher.Invoke(() => car.OffsetPos(-1, 0));
-            //  this.Dispatcher.Invoke(() => car.Rotate(-0.418));
-            //    Thread.Sleep(car.velocity+1);
-
-            //}
-            ////1890<i<=1990 y=-1; x=0.6;
-            //       r =-0.418
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    this.Dispatcher.Invoke(() => car.OffsetPos(-1, -0.6));
-            //    this.Dispatcher.Invoke(() => car.Rotate(-0.418));
-            //    Thread.Sleep(car.velocity+1);
-            //}
-            ////1990<i<=2090 y=-0.2; x=-1;
-            /// //       r =-0.418
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    this.Dispatcher.Invoke(() => car.OffsetPos(-0.2, -1));
-            //    this.Dispatcher.Invoke(() => car.Rotate(-0.418));
-            //    Thread.Sleep(car.velocity+1);
-
-            //}
-
-
-            ////2090<i<=2990 y=0; x=-1;
-
-            //for (int i = 0; i < 900; i++)
-            //{
-            //    this.Dispatcher.Invoke(() => car.OffsetPos(0, -1));
-            //    Thread.Sleep(car.velocity);
-
-            //       r =0
-            //}
         }
 
         public void RailRoadBot(Train train)
         {
             carStop = true;
+            Thread.Sleep(500);
             int offset;
             if (train.directionBot)
             {
@@ -405,7 +319,7 @@ namespace wpf_car_simulation
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            int velocity = Int32.Parse(BoundNumberTrain);
+            int velocity = VelocityConverter(Int32.Parse(BoundNumberTrain));
 
             Train train = new Train(myCanvas);
 
